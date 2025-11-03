fde93afdd8b2413c225e36841024a12f537c1a74
Galleto44 <johnnyvelasquezlezama44@gmail.com>
2025-11-02 21:26:32 -0600

    trate de conectar a postgres xd

--- ventas/views.py
+++ ventas/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Bienvenido {username}')
+                return redirect('index')  # vuelve a la página principal
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+        else:
+            messages.error(request, 'Datos inválidos')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
