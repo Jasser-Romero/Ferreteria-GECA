08f26444d8a8a3f9e617f3113fcf9c99049a9206
Jasser-Romero <jasseromero08@gmail.com>
2025-11-14 20:30:20 -0600

    feat: Added layout and created urls and templates

--- ventas/urls.py
+++ ventas/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('login/', views.user_login, name='login'),
+    path("logout/", views.user_logout, name="logout"),
     path("register/", views.user_register, name="register"),
+    path("dashboard/", views.index, name="dashboard"),
+    path("productos/", views.productos_lista, name="productos_lista"),
+    path("proveedores/", views.proveedores_lista, name="proveedores_lista"),
+    path("clientes/", views.clientes_lista, name="clientes_lista"),
+    path("ventas/", views.ventas_lista, name="ventas_lista"),
 ]
